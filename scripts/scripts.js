548a0dad5eb5d91651b8a1ef22194740c2784dc2
Ishan Saxena <isaxena171@gmail.com>
2015-11-07 19:28:55 +0530

    Closure

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,56 +6,60 @@ var count = {
 	'e': 0
 }
 
-$('#li1').click(function() {
-	$('.container').empty();
-	$('.container').append('<div id="a" ><h2>Pupu</h2><img src="img/cat1.jpg"><h1 id="1" ></h1></div>');
+for(var i = 1 ; i <= 5 ; i ++) {
+	$('#li' + i).click((function(i){
+		return function(){
+			$('.container').empty();
+			$('.container').append('<div id="a" ><h2>Pupu</h2><img src="img/cat'+i+'.jpg"><h1 id="1" ></h1></div>');
 
-	$('#1').text(count.a);
+			$('#1').text(count.a);
 
-	$('#a').click(function(){
-		// i++;
-		count.a++;
-		$('#1').text(count.a);
-	});
-});
+			$('#a').click(function(){
+				// i++;
+				count.a++;
+				$('#1').text(count.a);
+			});
+		}
+	})(i));
+}
 
-$('#li2').click(function() {
-	$('.container').empty();
-	$('.container').append('<div id="a" ><h2>Mimi</h2><img src="img/cat2.jpg"><h1 id="1" ></h1></div>');
-	$('#1').text(count.b);
-	$('#a').click(function(){
-		count.b++;
-		$('#1').text(count.b);
-	});
-});
+// $('#li2').click(function() {
+// 	$('.container').empty();
+// 	$('.container').append('<div id="a" ><h2>Mimi</h2><img src="img/cat2.jpg"><h1 id="1" ></h1></div>');
+// 	$('#1').text(count.b);
+// 	$('#a').click(function(){
+// 		count.b++;
+// 		$('#1').text(count.b);
+// 	});
+// });
 
 
-$('#li3').click(function() {
-	$('.container').empty();
-	$('.container').append('<div id="a" ><h2>Sese</h2><img src="img/cat3.jpg"><h1 id="1" ></h1></div>');
-	$('#1').text(count.c);
-	$('#a').click(function(){
-		count.c++;
-		$('#1').text(count.c);
-	});
-});
+// $('#li3').click(function() {
+// 	$('.container').empty();
+// 	$('.container').append('<div id="a" ><h2>Sese</h2><img src="img/cat3.jpg"><h1 id="1" ></h1></div>');
+// 	$('#1').text(count.c);
+// 	$('#a').click(function(){
+// 		count.c++;
+// 		$('#1').text(count.c);
+// 	});
+// });
 
-$('#li4').click(function() {
-	$('.container').empty();
-	$('.container').append('<div id="a" ><h2>Neni</h2><img src="img/cat4.jpg"><h1 id="1" ></h1></div>');
-	$('#1').text(count.d);
-	$('#a').click(function(){
-		count.d++;
-		$('#1').text(count.d);
-	});
-});
+// $('#li4').click(function() {
+// 	$('.container').empty();
+// 	$('.container').append('<div id="a" ><h2>Neni</h2><img src="img/cat4.jpg"><h1 id="1" ></h1></div>');
+// 	$('#1').text(count.d);
+// 	$('#a').click(function(){
+// 		count.d++;
+// 		$('#1').text(count.d);
+// 	});
+// });
 
-$('#li5').click(function() {
-	$('.container').empty();
-	$('.container').append('<div id="a" ><h2>Piki</h2><img src="img/cat5.jpg"><h1 id="1" ></h1></div>');
-	$('#1').text(count.e);
-	$('#a').click(function(){
-		count.e++;
-		$('#1').text(count.e);
-	});
-});
+// $('#li5').click(function() {
+// 	$('.container').empty();
+// 	$('.container').append('<div id="a" ><h2>Piki</h2><img src="img/cat5.jpg"><h1 id="1" ></h1></div>');
+// 	$('#1').text(count.e);
+// 	$('#a').click(function(){
+// 		count.e++;
+// 		$('#1').text(count.e);
+// 	});
+// });
